30b52ee0ea4722894186100e44e34f618b3a6b00
JDawnF <335825732@qq.com>
2019-04-14 23:43:38 +0800

    创建线程的方法，有返回值的线程及ReentranLock例子

--- src/thread/NewAThread.java
+++ src/thread/NewAThread.java
@@ -4,5 +4,34 @@ package thread;
  * @program: learning
  * @author: baichen
  **/
-public class NewAThread {
+public class NewAThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("重写run方法");
+    }
+
+    static class MyThread implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("MyThread.run()");
+        }
+    }
+    public static void main(String[] args) {
+        // 1.继承Thread类创建一个线程
+        NewAThread thread1=new NewAThread();
+        thread1.start();
+        // 2.实现一个Runnable创建一个线程
+        //启动 MyThread，需要首先实例化一个 Thread，并传入自己的 MyThread 实例:
+        MyThread myThread = new MyThread();
+        Thread thread = new Thread(myThread);
+        thread.start();
+        //事实上，当传入一个 Runnable target 参数给 Thread 后，Thread 的 run()方法就会调用
+//        target.run();
+//        public void run() {
+//            if (target != null) {
+//            }
+//            target.run();
+//        }
+    }
 }
--- src/thread/ReturnThread.java
+++ src/thread/ReturnThread.java
@@ -1,8 +1,66 @@
 package thread;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 /**
  * @program: learning
  * @author: baichen
+ * ExecutorService、Callable<Class>、Future 有返回值线程
+ * 有返回值的任务必须实现 Callable 接口，类似的，无返回值的任务必须 Runnable 接口。
+ * 执行 Callable 任务后，可以获取一个 Future 的对象，
+ * 在该对象上调用 get 就可以获取到 Callable 任务 返回的 Object 了，
+ * 再结合线程池接口 ExecutorService 就可以实现传说中有返回结果的多线程 了。
  **/
-public class ReturnThread {
+public class ReturnThread implements Callable {
+    public ReturnThread(String s) {
+        return;
+    }
+
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int taskSize = 10;
+        //创建定长线程池，可以控制线程的最大并发数，超出就在队列等待
+        ExecutorService pool = Executors.newFixedThreadPool(taskSize);
+        // 创建多个有返回值的任务
+        List<Future> list = new ArrayList<Future>();
+        for (int i = 0; i < taskSize; i++) {
+            Callable c = new ReturnThread(i + " ");
+            // 执行任务并获取 Future 对象
+            Future f = pool.submit(c);
+            list.add(f);
+        }
+        // 关闭线程池
+        pool.shutdown();
+        // 获取所有并发任务的运行结果
+        for (Future f : list) {
+            // 从 Future 对象上获取任务的返回值，并输出到控制台
+            System.out.println("res:"
+                    + f.get().toString());
+        }
+
+        // 基于线程池的方式
+        // 创建线程池
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        while (true) {
+            threadPool.execute(new Runnable() {
+                // 提交多个线程任务，并执行
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + " is running ..");
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
 }
+
